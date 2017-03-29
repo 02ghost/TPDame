6e196a02606028b12be8be7c7a87148ea92860af
Unknown <vizaar@hotmail.fr>
2017-03-29 08:54:38 +0200

    hello

--- Game.js
+++ Game.js
@@ -1,6 +1,5 @@
     var o="<table border=1>"
     var i=0;
-    var tab=['r','n','b','q','k','b','n','r','p'];
     var color;
     var img;
     for (let n=0;n<10;n++){
@@ -12,13 +11,17 @@
                 color = 'noir';
             img="";
             if(n==0)
-                img = '<img src="chess/b'+tab[b]+'.gif>"';
+                img = '<img src="Images/Npion.png">';
             if(n==1)
-               img = '<img src="chess/b'+tab[8]+'.gif>"';
-            if(n==7)
-                img = '<img src="chess/w'+tab[b]+'.gif>"';
-            if(n==6)
-                img = '<img onmouseover="action(this.id)" src="chess/w'+tab[8]+'.gif">';
+               img = '<img src="Images/Npion.png">';
+            if(n==2)
+                img = '<img src="Images/Npion.png">';
+            if(n==10)
+                img = '<img onmouseover="action(this.id)" src=""Images/Bpion.png">';
+            if(n==9)
+                img = '<img onmouseover="action(this.id)" src=""Images/Bpion.png">';
+            if(n==8)
+                img = '<img onmouseover="action(this.id)" src="Images/Bpion.png">';
             o=o+'<td id =c'+n+'-l'+b+' class = '+color+'>'+img+'</td>'; 
         }
         o+='</tr>'; 
