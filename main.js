1ac5322e677cb22f936017f364fccc401ab93fbb
Unknown <lolita020565@gmail.com>
2017-03-30 11:29:17 +0200

    transo pour mon petit arnaud

--- main.js
+++ main.js
@@ -13,21 +13,21 @@ window.onload =  function(){
             img="";
             if((n+b)%2==1){
                 if(n==0)
-                    img = '<img src="pn.png" width=70px height=70px>';
+                    img = '<img src="images/pn.png" width=70px height=70px>';
                 if(n==1)
-                    img = '<img src="pn.png" width=70px height=70px>';
+                    img = '<img src="images/pn.png" width=70px height=70px>';
                 if(n==2)
-                    img = '<img src="pn.png" width=70px height=70px >';
+                    img = '<img src="images/pn.png" width=70px height=70px >';
                 if(n==3)
-                    img = '<img src="pn.png" width=70px height=70px>';
+                    img = '<img src="images/pn.png" width=70px height=70px>';
                 if(n==6)
-                    img = '<img src="pb.png" width=70px height=70px>';
+                    img = '<img src="images/pb.png" width=70px height=70px>';
                 if(n==7)
-                    img = '<img src="pb.png" width=70px height=70px>';
+                    img = '<img src="images/pb.png" width=70px height=70px>';
                 if(n==8)
-                    img = '<img src="pb.png" width=70px height=70px>';
+                    img = '<img src="images/pb.png" width=70px height=70px>';
                 if(n==9)
-                    img = '<img src="pb.png" width=70px height=70px>';
+                    img = '<img src="images/pb.png" width=70px height=70px>';
             }
 
             o=o+'<td id =c'+n+'-l'+b+' class = '+color+'>'+img+'</td>'; 
